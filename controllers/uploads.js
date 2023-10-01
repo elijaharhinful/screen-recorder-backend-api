336e5c724671896e4080a3fc6199d4cc65bec630
elijaharhinful <elijaharhinful8@gmail.com>
2023-10-01 01:26:30 +0000

    used current working dir to serve videos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,28 +2,36 @@ import fs from "fs";
 import path from "path";
 import Video from "../models/Video.js"; // Import your video model
 
-const __dirname = path.dirname(import.meta.url);
-
 export const uploadVideo = async (req, res, next) => {
   try {
     const uniqueFilename = `video-${Date.now()}.mp4`; // Generate a unique filename
-    const fileStream = fs.createWriteStream(uniqueFilename, { flags: "a" });
+
+    // Ensure that the videos directory exists
+    const videosDir = path.join(process.cwd(), "videos");
+    if (!fs.existsSync(videosDir)) {
+      fs.mkdirSync(videosDir, { recursive: true });
+    }
+
+    const fileStream = fs.createWriteStream(
+      path.join(videosDir, uniqueFilename),
+      { flags: "a" }
+    );
 
     req.pipe(fileStream);
 
     fileStream.on("finish", () => {
       console.log("Chunk saved to disk");
-      const filePath = path.join(__dirname, uniqueFilename);
-      console.log("dirname: ", __dirname);
-      console.log('Location of the output file: ', filePath);
+      const filePath = path.join(videosDir, uniqueFilename);
+      console.log("dirname: ", process.cwd());
+      console.log("Location of the output file: ", filePath);
 
       // Get the server's base URL
-      const serverBaseUrl = req.protocol + '://' + req.get('host');
+      const serverBaseUrl = req.protocol + "://" + req.get("host");
 
       // Get the URL of the file
-      const fileUrl = serverBaseUrl + '/videos/' + uniqueFilename;
+      const fileUrl = serverBaseUrl + "/videos/" + uniqueFilename;
 
-      console.log('URL of the output file: ', fileUrl);
+      console.log("URL of the output file: ", fileUrl);
 
       // Save the URL to the stored video in a database here
       res.status(200).send("Chunk saved to disk");
