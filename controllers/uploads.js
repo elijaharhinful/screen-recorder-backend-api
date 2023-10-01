451263528bae19dc74bcf95961d46ecd00ca4fc2
elijaharhinful <elijaharhinful8@gmail.com>
2023-10-01 00:35:59 +0000

    fixed file name issues

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -14,6 +14,7 @@ export const uploadVideo = async (req, res, next) => {
     fileStream.on("finish", () => {
       console.log("Chunk saved to disk");
       const filePath = path.join(__dirname, uniqueFilename);
+      console.log("dirname: ", __dirname);
       console.log('Location of the output file: ', filePath);
       // Save the URL or local path to the stored video in a database here
       res.status(200).send("Chunk saved to disk");
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
 import { connectDB } from "./config/db.js";
 import { errorHandler } from "./middlewares/error.js";
-import uploadRoutes from './routes/uploads.js';
-import homeRoute from './routes/home.js';
+import uploadRoutes from "./routes/uploads.js";
+import homeRoute from "./routes/home.js";
 
 dotenv.config();
 
@@ -16,8 +18,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the "videos" directory
+app.use("/videos", express.static(path.join(__dirname, "videos")));
+
 // Routes
-app.use("/api", homeRoute)
+app.use("/api", homeRoute);
 app.use("/api/uploads", uploadRoutes);
 
 // Error handler route
